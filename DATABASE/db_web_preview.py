7475ef2ba6dd2b3f019cb554aed0cbcb33368b55
GetStuffDone <t.targetyt@gmail.com>
2020-08-30 21:36:23 +0000

    .

--- DATABASE/db_web_preview.py
+++ DATABASE/db_web_preview.py
@@ -6,11 +6,20 @@ class BACKEND_PREVIEW:
     def __init__(self):
         self.DB = sqlite3.connect('ChatDb.db')
         self.DB = self.DB.cursor()
+        self.is_running = True
     
     def _run_(self): 
-        information = self.DB.execute('SELECT SENT_CHAT FROM ChatDatabase')
-        print('running %s ' % information)
-        self.DB.commit()
+        self.DB.execute('SELECT * FROM ChatDatabase')
+
+        information = self.DB.fetchall()
+        print(information)
+        self.is_running = False
+    
+    def _is_running_(self): return self.is_running
+    
+    def _END_(self): return False
 
 BACK = BACKEND_PREVIEW()
-BACK._run_()
+
+while BACK._is_running_():
+    BACK._run_()
